58abe8ea04f792c08e0ad462e24ab22faf7aa57a
Andreas Edvardsen <andreasedvardsen@yahoo.no>
2017-10-03 13:13:48 +0200

    Added crossroad and start of MVP controller

---
+++ Trafikklys/Trafikklys/Crossroad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trafikklys
+{
+    class Crossroad
+    {
+        public CrossroadSide Top = new CrossroadSide();
+        public CrossroadSide Right = new CrossroadSide();
+        public CrossroadSide Bottom = new CrossroadSide();
+        public CrossroadSide Left = new CrossroadSide();
+    }
+}
---
+++ Trafikklys/Trafikklys/MVP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trafikklys
+{
+    class MVP
+    {
+        MVP()
+        { 
+            Crossroad crossroad = new Crossroad();
+            var car = new Car(crossroad.Bottom.Entrance, crossroad.Top.Exit);
+            
+        }
+    }
+}
