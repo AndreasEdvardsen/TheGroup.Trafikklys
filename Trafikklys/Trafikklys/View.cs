327a9d6981fcf860f4520a23ebc5363a5a191e47
Kevin andre hansen <kevin-a-hansen@live.no>
2017-10-04 10:22:42 +0200

    viser kø i de forskjellige felta

--- Trafikklys/Trafikklys/View.cs
+++ Trafikklys/Trafikklys/View.cs
@@ -8,9 +8,14 @@ namespace Trafikklys
 {
     class View 
     {
-        public View(Start start, Exit exit)
+        public View(Start start, Exit exit, Crossroad crossroad)
         {
         Console.WriteLine("Bilen kjørte fra " + start.Name + " til " + exit.Name);
+
+        Console.WriteLine("Det er " + crossroad.Top.Start.carList.Count + "biler i øverste felt");
+        Console.WriteLine("Det er " + crossroad.Right.Start.carList.Count + "biler i høyre felt");
+        Console.WriteLine("Det er " + crossroad.Bottom.Start.carList.Count + "biler i nederste felt");
+        Console.WriteLine("Det er " + crossroad.Left.Start.carList.Count + "biler i venstre felt");
         }
     }
 }
