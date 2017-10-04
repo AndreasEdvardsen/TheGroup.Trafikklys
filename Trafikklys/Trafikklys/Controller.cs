77f0d9699fcf3682542f556fd462c594ce2cc102
Truls Strand <truls13@live.no>
2017-10-04 10:18:50 +0200

    CreateCars

--- Trafikklys/Trafikklys/Controller.cs
+++ Trafikklys/Trafikklys/Controller.cs
@@ -8,6 +8,9 @@ namespace Trafikklys
 {
     class Controller
     {
+
+        Random random = new Random();
+
         public void SendCarToExit(Car car)
         {
             var trafficLight = car.Exit.TrafficLight.trafficLights;
@@ -21,5 +24,24 @@ namespace Trafikklys
                 View view = new View(car.Start, car.Exit);
             }
         }
+
+        public void CreateCars(Car car, Start start, Exit exit, Crossroad crossroad)
+        {
+            var startPoint = car.Start.Name;
+            var exitPoint = car.Exit.Name;
+
+            string[] sides = new string[4] {crossroad.Top.ToString(), crossroad.Right.ToString(), crossroad.Bottom.ToString(), crossroad.Left.ToString()};
+
+            startPoint = sides[random.Next(0, 3)];
+            exitPoint = sides[random.Next(0, 3)];
+
+            //hvis de er like, lag et nytt exitPoint
+            if (exitPoint == startPoint)
+            {
+                exitPoint = sides[random.Next(0, 3)];
+            }
+            
+        }
+
     }
 }
