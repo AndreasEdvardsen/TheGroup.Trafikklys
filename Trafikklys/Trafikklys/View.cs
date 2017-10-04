50586b1f8c39384acdfc8953e320af31618a2735
Kevin andre hansen <kevin-a-hansen@live.no>
2017-10-04 12:57:49 +0200

    lå til mellomrom

--- Trafikklys/Trafikklys/View.cs
+++ Trafikklys/Trafikklys/View.cs
@@ -15,10 +15,10 @@ namespace Trafikklys
         {
             Console.WriteLine("Bilen kjørte fra " + start.Name + " til " + exit.Name);
 
-            Console.WriteLine("Det er " + crossroad.Top.Start.carList.Count + "biler i øverste felt");
-            Console.WriteLine("Det er " + crossroad.Right.Start.carList.Count + "biler i høyre felt");
-            Console.WriteLine("Det er " + crossroad.Bottom.Start.carList.Count + "biler i nederste felt");
-            Console.WriteLine("Det er " + crossroad.Left.Start.carList.Count + "biler i venstre felt");
+            Console.WriteLine("Det er " + crossroad.Top.Start.carList.Count + " biler i øverste felt");
+            Console.WriteLine("Det er " + crossroad.Right.Start.carList.Count + " biler i høyre felt");
+            Console.WriteLine("Det er " + crossroad.Bottom.Start.carList.Count + " biler i nederste felt");
+            Console.WriteLine("Det er " + crossroad.Left.Start.carList.Count + " biler i venstre felt");
         }
     }
 }
