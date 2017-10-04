0fc15ab5a386a99c481c65a6232699a0db30b05b
Kevin andre hansen <kevin-a-hansen@live.no>
2017-10-04 10:10:30 +0200

    top, right, bottom og left crossroad count

--- Trafikklys/Trafikklys/Controller.cs
+++ Trafikklys/Trafikklys/Controller.cs
@@ -20,6 +20,23 @@ namespace Trafikklys
 
                 View view = new View(car.Start, car.Exit);
             }
+
+           
         }
+
+        public CrossroadSide CheckBiggestQueue(Crossroad crossroad)
+        {
+            var topRoad = crossroad.Top.Start.carList.Count;
+            var rightRoad = crossroad.Right.Start.carList.Count;
+            var bottomRoad = crossroad.Bottom.Start.carList.Count;
+            var leftRoad = crossroad.Left.Start.carList.Count;
+
+            if (topRoad > rightRoad && topRoad > bottomRoad && topRoad > leftRoad) return crossroad.Top;
+            if (rightRoad > bottomRoad && rightRoad > leftRoad && rightRoad > topRoad) return crossroad.Right;
+            if (bottomRoad > leftRoad && bottomRoad > topRoad && bottomRoad > rightRoad) return crossroad.Bottom;
+            if (leftRoad > topRoad && leftRoad > rightRoad && leftRoad > bottomRoad) return crossroad.Left;
+            return null;
+        }
+
     }
 }
